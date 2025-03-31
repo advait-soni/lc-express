2c633d2cd1e8713b309f5023e9fd2b23a76fc07e
advait-soni <advaitsoni2020@gmail.com>
2025-03-31 09:30:24 +0530

    mongo db connection

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 import express from "express"
 import "dotenv/config"
+import mongoose from "mongoose"
 
+const MONGO_URI = process.env.MONGO_URI
 const app = express()
 const PORT = process.env.PORT || 8000
 
+mongoose.connect(MONGO_URI).then(() => {
+    console.log("mongoDb")
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}).catch((err) => {
+    console.error(err)
+})
+
 app.use(express.json())
 
-app.post('/api/questions', (req, res) => {
-    console.log("req", req.body)
-    
-    res.end()
+app.post("/api/questions", (req, res) => {
+  console.log("req", req.body)
+
+  res.end()
 })
 
-app.get('/api/questions/:date', (req, res) => {
-    
-})
-
-
-app.listen(PORT , () => {
-    console.log(`Server running on port ${PORT}`)
-})
+app.get("/api/questions/:date", (req, res) => {})
